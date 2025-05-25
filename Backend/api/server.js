2e70ec4792c7c9d5ca469de63fa14e80679c878e
Codewithrasheed <rasheedahmed4845@gmail.com>
2025-05-25 10:11:38 +0500

    minor-change

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
 require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const userRoutes = require('./routes/userRoutes');  // adjust path accordingly
+const userRoutes = require('./routes/userRoutes');
 const idCardRoutes = require('./routes/idCardRoutes');
 const faqRoutes = require('./routes/faqRoutes');
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/id-cards', idCardRoutes);
 app.use('/api/faqs', faqRoutes);
 
-// MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error("MongoDB Error", err));
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error("MongoDB Connection Error",err));
 
-module.exports = app;
-module.exports.handler = serverless(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
